b740cbbf7c98360ea4f7471fa40793c9bd6037c8
Dalloriam <dalloriam@gmail.com>
2019-09-12 18:21:32 -0400

    Start work on cartography

---
+++ rogue/cartography/generation.go
@@ -0,0 +1,30 @@
+package cartography
+
+// A LevelTemplate implements logic for generating a single level (or "floor").
+type LevelTemplate interface {
+	Generate() Map
+}
+
+type LevelManager struct {
+	filePath string
+	levels   map[string]Map
+}
+
+func NewLevelManager(levelFilePath string) *LevelManager {
+	return &LevelManager{
+		filePath: levelFilePath,
+		levels:   make(map[string]Map),
+	}
+}
+
+func (m *LevelManager) Save() error {
+	return nil
+}
+
+func (m *LevelManager) Load() error {
+	return nil
+}
+
+// AddLevel
+func (m *LevelManager) AddLevel(name string, template LevelTemplate) {
+}
